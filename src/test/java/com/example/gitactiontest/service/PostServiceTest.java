33f4c67469fffecf393af7322511efbbbbe81325
unknown <wnsrl981228@gmail.com>
2023-01-08 00:21:14 +0900

    test2

--- src/test/java/com/example/gitactiontest/service/PostServiceTest.java
+++ src/test/java/com/example/gitactiontest/service/PostServiceTest.java
@@ -30,7 +30,7 @@ class PostServiceTest {
     void create() {
         int before = postRepository.size();
         postService.create(1, "제목");
-        assertThat(postRepository.size()).isEqualTo(before+1);
+        assertThat(postRepository.size()).isEqualTo(before+2);
     }
     // 테스트 중
 
